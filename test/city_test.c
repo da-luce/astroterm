7bdf16abdcc611321cc43f069a297e8b3a1eff05
Dalton Luce <102048662+da-luce@users.noreply.github.com>
2025-08-29 12:55:58 -0400

    fix: resolve GitHub issue #87 (#89)
    * Improve city generation script, now auto-downloads
    * Regenerate data/cities.csv (seems to resolve issue with Lisbon and some others... not sure why)
    * Add more city tests

--- test/city_test.c
+++ test/city_test.c
@@ -27,6 +27,24 @@ void test_get_city(void)
     TEST_ASSERT_EQUAL_FLOAT(-71.05977, city->longitude);
     free_city(city);
 
+    // Test for cities with duplicate names. The larger should maintain the same
+    // name
+    city = get_city("London");
+    TEST_ASSERT_NOT_NULL(city);
+    TEST_ASSERT_EQUAL_STRING("London", city->city_name);
+    TEST_ASSERT_EQUAL_FLOAT(51.50853, city->latitude);
+    TEST_ASSERT_EQUAL_FLOAT(-0.12574, city->longitude);
+    free_city(city);
+    // TODO: test other "London"
+
+    // Test for yet another city that exists
+    city = get_city("Lisbon");
+    TEST_ASSERT_NOT_NULL(city);
+    TEST_ASSERT_EQUAL_STRING("Lisbon", city->city_name);
+    TEST_ASSERT_EQUAL_FLOAT(38.72509, city->latitude);
+    TEST_ASSERT_EQUAL_FLOAT(-9.1498, city->longitude);
+    free_city(city);
+
     // Test for a city with mixed case and spaces (regression test for PR #79)
     city = get_city("Rio de Janeiro");
     TEST_ASSERT_NOT_NULL(city);
@@ -35,8 +53,20 @@ void test_get_city(void)
     TEST_ASSERT_EQUAL_FLOAT(-43.18223, city->longitude);
     free_city(city);
 
+    // Test for a city with non-ASCII characters
+    city = get_city("Thủ Dầu Một");
+    TEST_ASSERT_NOT_NULL(city);
+    TEST_ASSERT_EQUAL_STRING("Thủ Dầu Một", city->city_name);
+    TEST_ASSERT_EQUAL_FLOAT(10.9804, city->latitude);
+    TEST_ASSERT_EQUAL_FLOAT(106.6519, city->longitude);
+    free_city(city);
+
     // Test for a city that does not exist
     city = get_city("NonexistentCity");
+    TEST_ASSERT_NULL(city);
+
+    // Ensure small population locations are not present
+    city = get_city("Nantucket");
     TEST_ASSERT_NULL(city);
 
     // Test for a null input
